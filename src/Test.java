29f5a9ee58a667e3beb36c46ec13bd8fa4497f19
diegopliebana <4TiMebos>
2016-01-10 20:07:39 +0000

    no need for holefilling in enemycitadel

--- src/Test.java
+++ src/Test.java
@@ -82,7 +82,7 @@ public class Test
 
         //Game and level to play
         int gameIdx = 20;
-        int levelIdx = 0; //level names from 0 to 4 (game_lvlN.txt).
+        int levelIdx = 2; //level names from 0 to 4 (game_lvlN.txt).
         String game = gamesPath + games[gameIdx] + ".txt";
         String level1 = gamesPath + games[gameIdx] + "_lvl" + levelIdx +".txt";
         
