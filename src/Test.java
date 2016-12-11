834d4ebe331adbeb987dbeef1c6a2fe6ab0e3ec0
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2016-12-11 12:34:25 -0500

    Small Clean

--- src/Test.java
+++ src/Test.java
@@ -83,7 +83,7 @@ public class Test {
 	ArcadeMachine.playOneGame(game, level1, recordActionsFile, seed);
 
 	// 2. This plays a game in a level by the controller.
-//	 ArcadeMachine.runOneGame(game, level1, visuals, sampleRandomController, recordActionsFile, seed, 0);
+	// ArcadeMachine.runOneGame(game, level1, visuals, sampleRandomController, recordActionsFile, seed, 0);
 
 	// 3. This replays a game from an action file previously recorded
 	// String readActionsFile = recordActionsFile;
