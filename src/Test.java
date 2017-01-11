569c8fac3ee31ea176e11561f659b1af5d75c84d
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2017-01-11 17:46:47 +0200

    Just different tests

--- src/Test.java
+++ src/Test.java
@@ -67,7 +67,7 @@ public class Test {
 	int seed = new Random().nextInt();
 
 	// Game and level to play
-	int gameIdx = 0;
+	int gameIdx = 90;
 	int levelIdx = 0; // level names from 0 to 4 (game_lvlN.txt).
 	String game = gamesPath + games[gameIdx] + ".txt";
 	String level1 = gamesPath + games[gameIdx] + "_lvl" + levelIdx + ".txt";
@@ -80,10 +80,10 @@ public class Test {
 					// executed. null if not to save.
 
 	// 1. This starts a game, in a level, played by a human.
-	ArcadeMachine.playOneGame(game, level1, recordActionsFile, seed);
+	// ArcadeMachine.playOneGame(game, level1, recordActionsFile, seed);
 
 	// 2. This plays a game in a level by the controller.
-	// ArcadeMachine.runOneGame(game, level1, visuals, sampleRandomController, recordActionsFile, seed, 0);
+	ArcadeMachine.runOneGame(game, level1, visuals, sampleOLETSController, recordActionsFile, seed, 0);
 
 	// 3. This replays a game from an action file previously recorded
 	// String readActionsFile = recordActionsFile;
