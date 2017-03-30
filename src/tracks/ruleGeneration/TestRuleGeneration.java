e192f37a24838a86129d15c52d1930eda17d7231
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2017-03-30 18:04:47 -0400

    Fool proofing and some bug fixes

--- src/tracks/ruleGeneration/TestRuleGeneration.java
+++ src/tracks/ruleGeneration/TestRuleGeneration.java
@@ -61,7 +61,7 @@ public class TestRuleGeneration {
         String recordGameFile = generateRulePath + games[gameIdx] + "_ggame.txt";
         
         // 1. Generate rules (Interaction and Terminations) for a fixed level
-        if(RuleGenMachine.generateRules(game, level1, constructiveRuleGenerator, recordGameFile, seed)){
+        if(RuleGenMachine.generateRules(game, level1, randomRuleGenerator, recordGameFile, seed)){
             // RuleGenMachine.playOneGame(game, recordGameFile, level1, recordActionsFile, seed);
             RuleGenMachine.runOneGame(game, recordGameFile, level1, visuals, sampleMCTSController, recordActionsFile, seed, 0);
         }
