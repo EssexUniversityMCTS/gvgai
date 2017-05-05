49c5057cb10de54ec08ab187594d91391f271ec3
diegopliebana <4TiMebos>
2017-05-05 11:10:19 +0100

    gitignore

--- production/learningTrack2017/runClient_nocompile.sh
+++ production/learningTrack2017/runClient_nocompile.sh
@@ -2,7 +2,7 @@
 
 mkdir logs
 
-build_folder='build'
+build_folder='src/build'
 gson='gson-2.6.2.jar'
 #echo "java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -classpath ${build_folder}:${gson} JavaClient"
 java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -classpath ${build_folder}:${gson} JavaClient
