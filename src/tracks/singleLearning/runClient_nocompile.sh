a6476af2d1c4125cb45a56823fe334ef4838b2e0
diegopliebana <email@example.com>
2017-05-23 21:51:28 +0100

    fixing sh and iml

--- src/tracks/singleLearning/runClient_nocompile.sh
+++ src/tracks/singleLearning/runClient_nocompile.sh
@@ -5,7 +5,7 @@ if [ ! -d "$DIRECTORY" ]; then
   mkdir $DIRECTORY
 fi
 
-build_folder='clients/GVGAI-JavaClient/out/'
+build_folder='../../clients/GVGAI-JavaClient/out/'
 gson='gson-2.6.2.jar'
 #echo "java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -classpath ${build_folder}:${gson} JavaClient"
 java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -classpath ${build_folder}:${gson} JavaClient
