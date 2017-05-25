4ef6bdbca0695a899ce718c81825bf6ec402f2da
Jialin Liu <jialin.liu.cn@gmail.com>
2017-05-25 16:51:51 +0100

    correct bat

--- src/tracks/singleLearning/runClient_nocompile.bat
+++ src/tracks/singleLearning/runClient_nocompile.bat
@@ -4,10 +4,10 @@ set agent=%1
 set port=%2
 
 set path=%PATH%;C:\Program Files\Java\jdk1.8.0_121\bin
-set build_folder=..\..\..\client\GVGAI-JavaClient\out
-set src=..\..\..\clients\GVGAI-JavaClient\src
-set root_path=..\..\..\
-
+set build_folder=clients\GVGAI-JavaClient\out
+set src=clients\GVGAI-JavaClient\src
+set gson=lib\gson-2.8.0.jar
+IF NOT EXIST logs mkdir logs
 rem This script presumes that all the client-related Java files have been previously compiled and put in a folder called "build"
 
 rem Run the JavaClient class	
