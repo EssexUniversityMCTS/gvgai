8c71681185d182e18ca875085e60c8cf559358c3
Jialin Liu <ljialin@users.noreply.github.com>
2017-05-25 18:02:16 +0100

    update runClient_nocompile.bat
    update runClient_nocompile.bat, build client automatically

--- src/tracks/singleLearning/runClient_nocompile.bat
+++ src/tracks/singleLearning/runClient_nocompile.bat
@@ -5,10 +5,12 @@ set port=%2
 
 set path=%PATH%;C:\Program Files\Java\jdk1.8.0_121\bin
 set build_folder=clients\GVGAI-JavaClient\out
-set src=clients\GVGAI-JavaClient\src
+set src_folder=clients\GVGAI-JavaClient\src
 set gson=lib\gson-2.8.0.jar
-IF NOT EXIST logs mkdir logs
-rem This script presumes that all the client-related Java files have been previously compiled and put in a folder called "build"
+
+if not exist %build_folder% mkdir %build_folder%
+dir /s/b %src_folder%\*.java > sources.txt
+javac -cp %gson% -d %build_folder% @sources.txt
 
 rem Run the JavaClient class	
 java -agentlib:jdwp=transport=dt_socket,server=y,address=%port%,suspend=n -cp %build_folder%;%gson% JavaClient %agent%
