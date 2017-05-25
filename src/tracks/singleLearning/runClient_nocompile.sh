99318ce86b71a0a5eb0240068e3090f56009c373
Jialin Liu <jialin.liu.cn@gmail.com>
2017-05-25 18:09:43 +0100

    find the lost runClient_nocompile.sh

--- src/tracks/singleLearning/runClient_nocompile.sh
+++ src/tracks/singleLearning/runClient_nocompile.sh
@@ -1,11 +1,21 @@
-@echo off
+#!/bin/bash
 
-set path=%PATH%;C:\Program Files\Java\jdk1.8.0_121\bin
-set build_folder=..\..\..\client\GVGAI-JavaClient\out
-set src=..\..\..\clients\GVGAI-JavaClient\src
-set root_path=..\..\..\
+DIRECTORY=logs
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir $DIRECTORY
+fi
 
-rem This script presumes that all the client-related Java files have been previously compiled and put in a folder called "build"
+agent=$1
+port=$2
+src_folder='clients/GVGAI-JavaClient/src'
+build_folder='clients/GVGAI-JavaClient/out'
+gson='lib/gson-2.8.0.jar'
 
-rem Run the JavaClient class
-java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -cp %build_folder%;%gson% JavaClient
+mkdir -p $build_folder
+find ${src_folder} -name "*.java" > sources.txt
+javac -cp ${gson} -d $build_folder @sources.txt
+#echo java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -classpath ${build_folder}:${gson} JavaClient
+java -agentlib:jdwp=transport=dt_socket,server=y,address=${port},suspend=n -classpath ${build_folder}:${gson} JavaClient ${agent}
+
+#Uncomment this line instead for debug only (all printed out will go to output_redirect.txt. SERVER-CLIENT WON'T WORK
+#java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -classpath ${build_folder}:${gson} JavaClient > logs/output_redirect.txt
