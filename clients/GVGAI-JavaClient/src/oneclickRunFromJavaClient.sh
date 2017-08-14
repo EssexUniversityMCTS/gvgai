08b4fe7bc89f50a4e08ee41bc650844e7da0eafd
Jialin Liu <ljialin@users.noreply.github.com>
2017-08-14 15:05:38 +0100

    Update oneclickRunFromJavaClient.sh

--- clients/GVGAI-JavaClient/src/oneclickRunFromJavaClient.sh
+++ clients/GVGAI-JavaClient/src/oneclickRunFromJavaClient.sh
@@ -24,4 +24,4 @@ javac -d ${build_folder} @sources.txt
 # run with screen visualisation on
 #java -classpath ${build_folder} TestLearningClient -shDir ${shDir} -serverDir ${serverDir} -gameId ${gameId} -visuals
 # run without screen visualisation off
-java -classpath ${build_folder} TestLearningClient -shDir ${shDir} -serverDir ${serverDir} -gameId ${gameId} -agentName sampleLearner.Agent
+java -classpath ${build_folder} TestLearningClient -shDir ${shDir} -serverDir ${serverDir} -gameId ${gameId} -agentName sampleRandom.Agent
